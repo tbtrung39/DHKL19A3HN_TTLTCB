90cacfcf16bfcfaaa839856afbca1ec1370a2402
Do Ngoc Anh <dnanh.25174600174@sv.uneti.edu.vn>
2026-05-08 10:18:55 +0700

    ...

--- lab8/bai15.py
+++
@@ -1,9 +0,0 @@
-def binh_phuong_so_le():
-    n = int(input("Nhap n: "))
-    lst = [int(input(f"Nhap so thu {i+1}: ")) for i in range(n)]
-    
-    ket_qua = list(map(lambda x: x**2, filter(lambda x: x % 2 != 0, lst)))
-    
-    print("Binh phuong cac so le trong list la:", ket_qua)
-
-binh_phuong_so_le()
